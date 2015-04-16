934aa508bdc1260d906ebf68fc95b924724d92f6
Izzy Benavente <ibenaven@usc.edu>
2015-04-16 15:37:09 -0700

    created menu overlay, filled out contact page, made a landing page

--- js/effects.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $(".splash-logo").fadeIn("slow");
-    $(".splash-logo").hover(function(){
-         $(".landing").css("background-image", "url(images/Strip_whiteoverlay.jpg)");
-    });
-});
