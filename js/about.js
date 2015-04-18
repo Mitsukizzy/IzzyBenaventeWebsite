7cc54c8aadf4bf83cfb655386d7e62c847a1f2e4
Izzy Benavente <ibenaven@usc.edu>
2015-04-18 02:26:07 -0700

    Finished about page! Currently just basic info, might add education or resume later

--- js/about.js
+++ js/about.js
@@ -3,4 +3,24 @@ $(document).ready(function() {
     $('.about-more').hide();
     $('.about-izzy').fadeIn(1000);
     $('.about-more').delay(1000).slideDown(500);
+    
+    $(function () {
+        var $win = $(window);
+        var $atTop = true;
+        
+
+        $win.scroll(function () {
+            if ($win.scrollTop() != 0 && $atTop)
+            {
+                $('.about-izzy').fadeOut();
+                $('.about-more').slideUp();
+                $atTop = false;
+            }
+            else if($win.scrollTop() == 0 && !$atTop){
+                $('.about-izzy').fadeIn();
+                $('.about-more').slideDown();
+                $atTop = true;   
+            }
+        });
+     }); 
 });
