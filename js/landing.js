13ace91f09ad36e4fa77df76baafc1d933c32530
Izzy Benavente <ibenaven@usc.edu>
2015-04-19 01:31:47 -0700

    Created new images for Works page, initial layout done

--- js/landing.js
+++ js/landing.js
@@ -2,20 +2,18 @@ $(document).ready(function() {
     $('.name-first').hide();
     $('.name-last').hide();
     $('.info-list').hide();
-    $('.footer').hide();
-    $('.menu-item').hide();
+    $('.menu-icon').hide();
     
     $('.name-first').fadeIn(1000);
     $('.name-last').fadeIn(1000);
     $('.info-list').delay(1000).fadeIn(2000, function(){        
         $('.landing-item').delay(750).each(function(i){
-            $(this).delay(1000 * i).fadeIn();
+            $(this).delay(750 * i).fadeIn();
         });
         
     });
     
-    $('.footer').delay(6500).slideDown();
-    $('.menu-item').delay(6500).fadeIn();
+    $('.menu-icon').delay(5500).fadeIn();
     
     $('.landing-item span').hover(function(event) {
         $(this).addClass("grey-line");
