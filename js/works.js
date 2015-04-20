0d1eb1b4b917d0daf162a6f452da1faa3e0f6be7
Izzy Benavente <ibenaven@usc.edu>
2015-04-19 23:44:41 -0700

    Added some temporary links for the works page to go to

--- js/works.js
+++ js/works.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
-    
-    
+    $("div.works-item").click(
+        function(event)
+        {
+            //window.location = $(this).attr("url");
+            $url = $(this).attr("url");
+            window.open($url,'_blank');
+            event.preventDefault();
+        }
+    );
 });
