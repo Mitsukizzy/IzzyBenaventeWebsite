d48a184d23daf353b2b6f0186facee9499627f51
Izzy Benavente <ibenaven@usc.edu>
2016-02-23 23:23:06 -0800

    Moving some in-progress work from laptop to desktop

--- js/onepage.js
+++ js/onepage.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
             resizeBackground();
             
             // If scrolled to bottom, show bottom bar, +2 so it'll just touch the bottom
-            if($(window).scrollTop() + $(window).height() + 1 >= $(document).height() && !$atBottom)  {
+          /*  if($(window).scrollTop() + $(window).height() + 1 >= $(document).height() && !$atBottom)  {
                 $('.footer').slideDown();
                 $atBottom = true;
             }
             else if($(window).scrollTop() + $(window).height() != $(document).height() && $atBottom)  {
                 $('.footer').slideUp();
                 $atBottom = false;
-            }
+            }*/
             
             if($(window).scrollTop() <= $('#about').height()  && !$atTop)  {
                 $('.custom-wrapper').fadeOut("slow");
