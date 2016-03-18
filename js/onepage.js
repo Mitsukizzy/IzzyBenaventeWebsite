1992b6ab2a78c954ec6c0023399ed0d54c4cc0f0
Izzy Benavente <ibenaven@usc.edu>
2016-03-18 16:42:20 -0700

    Huge update: Major restyles, ripped out old js and css, addition of Abridged CV and slideshow

--- js/onepage.js
+++ js/onepage.js
@@ -3,12 +3,6 @@ $(document).ready(function() {
     $('.landing-item').delay(250).each(function(i){
         $(this).delay(750 * i).css({'opacity':0}).animate({'opacity':1}, 1000);
     });        
-        
-    $('.landing-item span').hover(function(event) {
-        $(this).addClass("grey-line");
-    },function(){
-        $(this).removeClass("grey-line");     
-    });
 
     //jQuery for page scrolling feature - requires jQuery Easing plugin
     $(function() {
@@ -22,27 +16,26 @@ $(document).ready(function() {
         });
     });
     
-    // Swap through images on landing background
-    /*var i = 0;
-    function ChangeLandingBG() {
-        if( i == 0 )
-        {
-            $('#BG1').css('background', 'url(http://izzybenavente.me/images/strip.jpg) 50% 0 no-repeat fixed');
-            $('#BG1').css('background-size', 'cover');
-        }
-        else if( i == 1 )
-        {
-            $('#BG1').css('background', 'url(http://izzybenavente.me/images/home.jpg) 50% 0 no-repeat fixed');
-            $('#BG1').css('background-size', 'cover');
-        }
+    // Swap through images
+    var images = [
+        "http://izzybenavente.me/gallery/ggames.jpg",
+        "http://izzybenavente.me/gallery/treehacks.jpg",
+        "http://izzybenavente.me/gallery/acm.jpg",
+        "http://izzybenavente.me/gallery/dragonboat.jpg",
+        "http://izzybenavente.me/gallery/mobilehack.jpg"   
+    ];
+    var i = 0;    
+    function slideShow() {
+        $('#slideshow').className += "fadeOut";
+        setTimeout(function() {
+            $('#slideshow').css('background', 'url(' + images[i] + ')');
+            $('#slideshow').className = "";
+        }, 1000);
         i++;
-        if(i>1) 
-        {
-            i = 0;
-        }
-       setTimeout(ChangeLandingBG, 5000);
+        if (i == images.length) { i = 0; }
+        setTimeout(slideShow, 4000);
     }
-    setTimeout(ChangeLandingBG, 5000);*/
+    slideShow();
     
     function closeTopNav() {
         $('.custom-wrapper').fadeOut("slow");    
@@ -66,14 +59,14 @@ $(document).ready(function() {
     // Show and hide the top bar nav
     var $win = $(window);       
     $win.scroll(function () {           
-        if($(window).scrollTop() <= $('#about').height() && $('.custom-wrapper').is(":visible"))  {
+        if($(window).scrollTop() <= $('.profile-image1').height() + 300 && $('.custom-wrapper').is(":visible"))  {
             if(!elevator.elevating() && !elevator_no.elevating())
             {
                 // Extra check because elevator can cause incorrect scroll position due to dynamic resizing of content such as BG images
                 $('.custom-wrapper').fadeOut("slow");       
             }
         }
-        else if($(window).scrollTop() > $('#about').height() && $('.custom-wrapper').is(":hidden"))  {
+        else if($(window).scrollTop() > $('.profile-image1').height() + 300 && $('.custom-wrapper').is(":hidden"))  {
             $('.custom-wrapper').fadeIn("slow");
         }
     });
@@ -82,12 +75,10 @@ $(document).ready(function() {
         $workitem = $(this).attr("item");
         $('.item-' + $workitem).fadeIn();
         $('body').addClass('noscroll');     // prevent body from scrolling
-        $('.menu-icon').hide();             // hide hamburger
         
         $('.close-icon-work').click(function() {
             $('.item-' + $workitem).fadeOut(); 
             $('body').removeClass('noscroll');  
-            $('.menu-icon').show();
         });
     });
     
@@ -97,29 +88,11 @@ $(document).ready(function() {
         $('.item-' + $nextitem).fadeIn();
         $('.item-' + $curitem).fadeOut();
         $('body').addClass('noscroll');     // prevent body from scrolling
-        $('.menu-icon').hide();             // hide hamburger
         
         $('.close-icon-work').click(function() {
             $('.item-' + $nextitem).fadeOut(); 
             $('body').removeClass('noscroll');  
-            $('.menu-icon').show();
         });
     });
-                
-    // Responsive icon resizing
-    if($(window).width() <= 610) {
-        $("fa-4x").each(function() {
-            $(this).removeClass("fa-4x");
-            $(this).addClass("fa-2x");
-        });
-    }
-    
-    $(window).resize(function() {
-        if($(window).width() <= 610) {
-            $("fa-4x").each(function() {
-                $(this).removeClass("fa-4x");
-                $(this).addClass("fa-2x");
-            });
-        }
-    });
+        
 });
